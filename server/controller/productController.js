633fa857e4aa4e68107b9322201b722b321054fe
naimursafi2 <naimursafi4@gmail.com>
2026-06-05 17:59:26 +0600

    skus

--- server/controller/productController.js
+++ server/controller/productController.js
@@ -37,6 +37,30 @@ const createProduct = async (req, res) => {
       return res.status(400).send({ message: "Invalid Category" });
     if (!price)
       return res.status(400).send({ message: "Product price is required" });
+
+    //validation of product variants
+    const variantsData = variants;
+    if (!Array.isArray(variantsData) || variantsData.length === 0)
+      return res
+        .status(400)
+        .send({ message: "Minimum 1 variants is required" });
+
+    for (const variant of variantsData) {
+      if (!variant.sku)
+        return res.status(400).send({ message: "SKU is required" });
+      if (!variant.color)
+        return res.status(400).send({ message: "color is required" });
+      if (!variant.size)
+        return res.status(400).send({ message: "size is required" });
+      if (!variant.stock || variant.stock < 1)
+        return res
+          .status(400)
+          .send({ message: "stock is required and must be more than o" });
+    }
+
+    const skus = variantsData.map((v) => v.sku);
+    if (new Set(sku).size !== skus.length)
+      return res.status(400).send({ message: "Sku must be unique" });
   } catch (error) {
     console.log(error);
     res.status(400).send({ message: "Inernal Server Error" });
