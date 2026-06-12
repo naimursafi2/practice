19f88e627b99fe7326ffbd365c9db7e5e550f458
naimursafi2 <naimursafi4@gmail.com>
2026-06-12 18:18:38 +0600

    multer

--- server/routes/productRoute.js
+++ server/routes/productRoute.js
@@ -1,8 +1,19 @@
-const express = require('express');
-const { createProduct } = require('../controller/productController');
-const { authMiddleware, roleCheck } = require('../middleware/authMiddleware');
+const express = require("express");
+const multer = require("multer");
+const upload = multer();
+const { createProduct } = require("../controller/productController");
+const { authMiddleware, roleCheck } = require("../middleware/authMiddleware");
 const route = express.Router();
 
-route.post("/create",authMiddleware,roleCheck(["admin","moderator"]),createProduct)
+route.post(
+  "/create",
+  authMiddleware,
+  roleCheck(["admin", "moderator"]),
+  upload.fields([
+    { name: "thumbnail", maxCount: 1 },
+    { name: "images", maxCount: 4 },
+  ]),
+  createProduct,
+);
 
-module.exports = route
+module.exports = route;
